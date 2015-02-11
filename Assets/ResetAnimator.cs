ad3a1dcd964a8a8bde828669e469e73bb83c2d5d
tel <t_telkun@hotmail.com>
2015-02-12 02:40:35 +0900

    サンプル作成

---
+++ Assets/ResetAnimator.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ResetAnimator : MonoBehaviour
+{
+	Animator _animator;
+
+	void Awake()
+	{
+		_animator = GetComponent<Animator>();
+	}
+
+	void Update()
+	{
+		if (Input.GetMouseButtonDown(0))
+		{
+			var info = _animator.GetCurrentAnimatorStateInfo(0);
+			_animator.Play(info.nameHash, 0, 0.0f);
+		}
+	}
+}
